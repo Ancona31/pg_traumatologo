06df88f21c615f92b58dd154996f3031d962ee04
Ancona31 <aancona@gmail.com>
2026-03-08 20:05:32 -0600

    Corrige la clave supabase anon jwt

--- admin/js/supabase-client.js
+++ admin/js/supabase-client.js
@@ -4,8 +4,8 @@
        con los valores reales de tu proyecto en supabase.com
 ================================================================ */
 
-const SUPABASE_URL  = 'https://TU_PROYECTO.supabase.co';        // TODO: reemplazar
-const SUPABASE_ANON = 'TU_ANON_KEY';                            // TODO: reemplazar
+const SUPABASE_URL  = 'https://ithwmacwyexinxjtnbcg.supabase.co';        // TODO: reemplazar
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0aHdtYWN3eWV4aW54anRuYmNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMTYyNTYsImV4cCI6MjA4ODU5MjI1Nn0.WbG3MkP7xvI64V5Ip_GEBkKpVIMs-D8e7fOGB8eePfE';                            // TODO: reemplazar
 
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_ANON);
